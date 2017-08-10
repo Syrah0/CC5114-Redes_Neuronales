b5a6c57dc7b40ca9118809ed0e09362445ca3f51
Alexandra <alexasofia@hotmail.com>
2017-08-09 22:49:25 -0400

    modified code and graph

--- MiniTarea2/Training.py
+++ MiniTarea2/Training.py
@@ -1,6 +1,6 @@
 import random
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve
+# from sklearn.metrics import roc_curve
 
 ### Alumna: Alexandra Ibarra
 
@@ -27,53 +27,18 @@ class Perceptron:
             return 0
         return 1
 
+    ## Funcion auxiliar encargada para calculo de la clase de los elementos
     def funAux(self,x):
         return 2*x + 5
 
+    ## Funcion encargada del proceso de entrenamiento del perceptron
+    ## Esta recibe como parametro la cantidad de iteraciones que se desean realizar para entrenar
     def training(self,iter):
         C = 0.01
-        realValues = []
-        predictValues = []
-        goodArray = []
-        badArray = []
-        good = 0
-        bad = 0
-        for j in range(iter):
-            x1 = random.randint(-30,30)
-            y1 = random.randint(-30,30)
-            if(self.funAux(x1)>y1):
-                output = 0
-            else:
-                output = 1
-            realValues.append(output)
-            predict = self.perceptron(x1, y1)
-            predictValues.append(predict)
-            if(predict == 0 and output == 1):
-                bad += 1
-                self.w1 = self.w1 + C * x1
-                self.w2 = self.w2 + C * y1
-            elif(predict == 1 and output == 0):
-                bad += 1
-                self.w1 = self.w1 - C * x1
-                self.w2 = self.w2 - C * y1
-            else:
-                good += 1
-            goodArray.append(good)
-            badArray.append(bad)
-        return [goodArray,badArray,realValues,predictValues]
-    ### RETORNAR ARRAYS Y CREAR FUNCION TEST QUE GRAFIQUE
-
-    def prediction(self,iter):
-        goodArray = []
-        badArray = []
-        realValues = []
-        predictValues = []
-        class0x = []
-        class0y = []
-        class1x = []
-        class1y = []
-        good = 0
-        bad = 0
+       # realValues = [] # contiene los valores o clases reales de los puntos
+       # predictValues = [] # contiene las clases predichas por el perceptron
+        goodArray = [] # contiene la cantidad de elementos buenas tras cada iteracion
+        good = 0 # cantidad de clases correctamente acertadas
         for j in range(iter):
             x1 = random.randint(-50,50)
             y1 = random.randint(-50,50)
@@ -81,68 +46,97 @@ class Perceptron:
                 output = 0
             else:
                 output = 1
-            realValues.append(output)
+            #realValues.append(output)
+            predict = self.perceptron(x1, y1)
+            #predictValues.append(predict)
+            if(predict == 0 and output == 1):
+                self.w1 = self.w1 + C * x1
+                self.w2 = self.w2 + C * y1
+            elif(predict == 1 and output == 0):
+                self.w1 = self.w1 - C * x1
+                self.w2 = self.w2 - C * y1
+            else:
+                good += 1
+            goodArray.append(good)
+            #print(self.w1, self.w2)
+        return goodArray
+
+    def prediction(self,iter):
+        #realValues = []  # contiene los valores o clases reales de los puntos
+        #predictValues = []  # contiene las clases predichas por el perceptron
+        goodArray = []  # contiene la cantidad de elementos buenas tras cada iteracion
+        good = 0  # cantidad de clases correctamente acertadas
+        class0x = [] # valor de X perteneciente a la clase 0
+        class0y = [] # valor de Y perteneciente a la clase 0
+        class1x = [] # valor de X perteneciente a la clase 1
+        class1y = [] # valor de Y perteneciente a la clase 1
+        for j in range(iter):
+            x1 = random.randint(-50,50)
+            y1 = random.randint(-50,50)
+            if(self.funAux(x1)>y1):
+                output = 0
+            else:
+                output = 1
+            #realValues.append(output)
             predict = self.perceptron(x1,y1)
-            predictValues.append(predict)
+            #predictValues.append(predict)
             if(predict == 0):
                 class0x.append(x1)
                 class0y.append(y1)
             else:
                 class1x.append(x1)
                 class1y.append(y1)
-            if(predict == 0 and output == 1):
-                bad += 1
-            elif(predict == 1 and output == 0):
-                bad += 1
-            else:
+            if((predict == 0 and output == 0) or (predict == 1 and output == 1)):
                 good += 1
             goodArray.append(good)
-            badArray.append(bad)
-            ##print bad,good
-        return [goodArray,badArray,realValues,predictValues,class0x,class0y,class1x,class1y]
+        return [goodArray,class0x,class0y,class1x,class1y]
 
+    # Funcion encargada de testear el entrenamiento del perceptron
+    # grafica el rendimiento del entrenamiento
     def testTraining(self):
         ### Entrenamiento ###
-        iter = 2000
-        [good,bad,real,pred] = self.training(iter)
+        iter = 1000
+        good = self.training(iter)
         x = []
         for j in range(iter):
             good[j] = (good[j]*1.0) / (j + 1)
-            bad[j] = (bad[j]*1.0) / (j + 1)
             x.append(j)
         plt.figure()
+        plt.title("Rendimiento del entrenamiento")
+        plt.ylabel("Cantidad de puntos correctamente clasificados")
+        plt.xlabel("N° iteraciones")
         plt.plot(x,good)
-        #plt.figure()
-        #plt.plot(x,bad)
         plt.ylim([0.0,1.05])
-        plt.figure()
-        fp, tp, th = roc_curve(real, pred)
-        plt.plot(fp, tp)
         plt.show()
 
+    # Funcion encargada de testear el rendimiento del perceptron ya entrenado
+    # grafica el rendimiento del perceptron con un nuevo conjunto (prueba)
+    # grafica la distribucion de los nuevos puntos en el espacio
     def testPrediction(self):
-        ### Entrenamiento ###
-        iter = 400
-        [good,bad,real,pred,c0x,c0y,c1x,c1y] = self.prediction(iter)
+        ### Testing con conjunto de prueba ###
+        iter = 1000
+        [good,c0x,c0y,c1x,c1y] = self.prediction(iter)
         x = []
         for j in range(iter):
             good[j] = (good[j]*1.0) / (j + 1)
-            bad[j] = (bad[j]*1.0) / (j + 1)
             x.append(j)
         plt.figure()
-        plt.plot(x,good)
-        #plt.figure()
-        #plt.plot(x,bad)
+        plt.title("Rendimiento del entrenamiento fase de testing")
+        plt.ylabel("Cantidad de puntos correctamente clasificados")
+        plt.xlabel("N° iteraciones")
+        plt.plot(x,good,'-')
         plt.ylim([0.0, 1.05])
         plt.figure()
         plt.plot(c0x, c0y, 'or')
         plt.plot(c1x, c1y, 'ob')
         plt.plot([-50,50], [self.funAux(-50),self.funAux(50)])
-        plt.figure()
-        fp, tp, th = roc_curve(real, pred)
-        plt.plot(fp,tp)
+        #plt.figure()
+        #plt.title("Curva de ROC del rendimiento del perceptron en testing")
+        #fp, tp, th = roc_curve(real, pred)
+        #plt.plot(fp,tp)
         plt.show()
 
-p = Perceptron(10,1,-2.5)
+# crea perceptron para proceso de testeo
+p = Perceptron(-2.5,1.5,-0.5)
 p.testTraining()
 p.testPrediction()
---
+++ MiniTarea3/README.txt
@@ -0,0 +1,64 @@
+El link directo al repositorio que contiene la tarea es:
+
+https://github.com/Syrah0/CC5114-Redes_Neuronales/tree/master/MiniTarea1
+
+IMPORTANTE!!!!
+
+Esta carpeta contiene 1 archivo escrito en lenguaje Python 3.5.3
+Además es posible observar una carpeta de resultados donde se encuentran algunas imágenes con los resultados obtenidos tras variar el valor de C.
+
+Es IMPORTANTE señalar que para ejecutar el código se necesita de la librería:  Matplotlib 
+Esta permite graficar y visualizar los diferentes gráficos realizados.
+
+MODO DE EJECUCIÓN Y COMPILACIÓN
+
+En primer lugar se deben descargar los archivos del repositorio ya sea clonando el repositorio de github con el comando:
+
+git clone https://github.com/Syrah0/CC5114-Redes_Neuronales.git
+
+o de manera manual copiando los archivos de github a un archivo propio (copy and paste).
+
+
+Las siguientes instrucciones se darán considerando que se utiliza PyCharm dado que este facilita la instalación de la librería deseada
+
+1.- Teniendo los archivos se debe abrir PyCharm.
+
+2.- Luego se debe ir a la opción "File" y poner "Open...".
+
+3.- Buscar la carpeta o archivo que se desea ejecutar (es recomendable seleccionar la carpeta que contiene el archivo y hacer click en "OK", en caso contrario seleccionar el archivo Training.py y luego "OK")
+
+4.- Una vez abierto el archivo seleccionado hacer click derecho sobre el nombre del archivo (o en el archivo como tal) y seleccionar "Run 'Training.py'".
+
+Es importante señalar que en caso que no se haya configurado un interprete para el lenguaje se deberá configurar uno, para eso se debe ir a File -> Settings y seleccionar el "Project Interpreter". NOTAR QUE EL INTERPRETE DEBE TENER INSTALADA LA LIBRERIA MATPLOTLIB (sino ir al signo + e instalarla) 
+
+Tras lo anterior aparecerán los gráficos tras el entrenamiento del perceptrón.
+
+
+NOTA!!!!!!!!!
+
+Tambien es posible realizar lo mismo a través de la consola de python (Python Console). Esto será recomendable en caso que se deseen ejecutar las funciones por separado. 
+Para esto se debe ir, en PyCharm, a Python Console.
+Luego poner:
+
+from <Nombre/Carpeta/Archivo>.Training import *
+
+Por ejemplo, si Training.py está de Tarea entonces poner: from Tarea.Training import *
+
+IMPORTANTE!!!! EL NOMBRE DE LA CARPETA SE PONE SI Y SOLO SI DICHA CARPETA ESTÁ CONTENIDA DENTRO DE LA CARPETA DEL PROYECTO. EN EL EJEMPLO ANTERIOR ES EQUIVALENTE A DECIR QUE:
+
+Proyect
+ |--> Tarea
+	|--> Training.py
+
+EN CASO CONTRARIO ES SOLO from Training import *
+
+Tras lo anterior se ejecutará automaticamente el proceso de prueba escrito en el código y la generación de los gráficos. En caso de probar las otras funciones basta con colocar:
+
+p.perceptron(x1,x2)
+
+En dicho caso se usará el perceptrón entrenado. Si se desea usar otro perceptron entonces usar:
+
+p2 = Perceptron(w1,w2,bias)
+p2.perceptron(x1,x2)
+
+por ejemplo.
